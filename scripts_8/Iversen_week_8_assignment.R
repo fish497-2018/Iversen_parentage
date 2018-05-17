34bc327de638434b5f972a2f6e7c4202f79ecc15
Your Name <eki2@uw.edu>
2018-05-17 10:08:56 -0700

    filtered NAs and blanks from data

--- scripts_8/Iversen_week_8_assignment.R
+++ scripts_8/Iversen_week_8_assignment.R
@@ -5,8 +5,12 @@ library(dplyr)
 
 parentage_data <- read.csv("data_8/parentage_with_phenotypes_CSV.csv")
 
-#Removing blanks and NAs from the data frame: 
-parentage_data_no_NAs <- parentage_data[!(is.na(parentage_data) | parentage_data== ""), ]
-View(parentage_data_no_NAs)
+#Filtering out NAs and blanks by selecting sex columns only with m or f 
+filtered_parentage_data <- parentage_data %>%
+  select(sex, length, depth, sex.1, length.1, depth.1, sex.2, length.2, depth.2) %>%
+  filter(sex == "m" | sex== "f") %>% 
+  filter(sex.1 == "m" | sex.1 =="f") %>% 
+  filter(sex.2 =="m" | sex.2 == "f")
 
-#Bleh, I can't get this to work yet...
+head(filtered_parentage_data)#Bleh, I can't get this to work yet...
+View(filtered_parentage_data)
